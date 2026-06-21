95b149356da790be35a2efc74957d1ef775673d8
ShevRuslan1 <kv4rxvftk2@privaterelay.appleid.com>
2026-06-21 17:53:10 -0400

    Remove logo from title bar

--- apps/renderer/src/renderer/src/components/layout/TitleBar.tsx
+++ apps/renderer/src/renderer/src/components/layout/TitleBar.tsx
@@ -100,20 +100,6 @@ export function TitleBar() {
         zIndex: 4,
       }}
     >
-      {/* Logo */}
-      <div style={{ marginLeft: 12, display: 'flex', alignItems: 'center', gap: 8 }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-110 -110 220 220" width={16} height={16} style={{ flexShrink: 0 }}>
-          <polygon points="0,-92 14,0 0,92 -14,0" fill="#5316D4"/>
-          <polygon points="0,-92 14,0 0,92 -14,0" fill="#3D0FA3" transform="rotate(30)"/>
-          <polygon points="0,-92 14,0 0,92 -14,0" fill="#8A52FF" transform="rotate(60)"/>
-          <polygon points="0,-92 14,0 0,92 -14,0" fill="#3D0FA3" transform="rotate(90)"/>
-          <polygon points="0,-92 14,0 0,92 -14,0" fill="#5316D4" transform="rotate(120)"/>
-          <polygon points="0,-92 14,0 0,92 -14,0" fill="#8A52FF" transform="rotate(150)"/>
-          <circle r="24" fill="#1B044F"/>
-          <circle r="6" fill="#ECE4FF"/>
-        </svg>
-      </div>
-
       <div style={{ flex: 1 }} />
 
       {/* Update chip */}
